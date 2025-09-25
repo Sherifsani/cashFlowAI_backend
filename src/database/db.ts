0cb7a61b3133af8b56c58c74f3e861d3b4f5c00d
Sherif Sani <sanisherif11@gmail.com>
2025-09-25 16:14:40 +0100

    add DynamoDB client setup and commands for database interactions

---
+++ src/database/db.ts
@@ -0,0 +1,9 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, PutCommand, GetCommand } from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient({
+    region : process.env.AWS_REGION
+})
+const dynamo = DynamoDBDocumentClient.from(client);
+
+export { dynamo, PutCommand, GetCommand };
